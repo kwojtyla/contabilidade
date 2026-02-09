93e16bd6ec2a235099adc8a1ebaa65d739e97eb9
Karol Wojtyla <karolwojtyla360@gmail.com>
2026-02-09 03:43:19 -0300

    feat: add icons from figma

---
+++ src/assets/icons/play.tsx
@@ -0,0 +1,26 @@
+import type { IconProps } from "@/types/icons";
+
+export const PlayIcon: React.FC<IconProps> = ({
+  width,
+  height,
+  fill,
+  stroke,
+}) => {
+  return (
+    <svg
+      width={width || "18"}
+      height={height || "21"}
+      viewBox="0 0 18 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 10.093V1L8.875 5.5465L16.75 10.093L8.875 14.6395L1 19.186V10.093Z"
+        fill={fill || "black"}
+        stroke={stroke || "black"}
+        strokeWidth="2"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
